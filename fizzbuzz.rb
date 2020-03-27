4ead9ca2c897d2724b44660c35f2846ebbe8d496
Matthew Hallquist <matthallquist@yahoo.com>
2020-03-27 22:48:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
+ if number / 3
 
+ elsif number / 5
+
+ else
+   return nil
+ end
 end
